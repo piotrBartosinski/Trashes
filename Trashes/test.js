261fb2649384eb8a7eb6d9563658dd64b6d3c6e0
Piotr Bartosinski <piotr.bartosinski@outlook.com>
2019-11-17 00:12:04 +0100

    Changes 3

--- Trashes/test.js
+++ Trashes/test.js
@@ -9,6 +9,7 @@ import {
 
 import React, {Component} from "react";
 import styles from './styles/style.js';
+import trashesList from './trashes_list';
 
 export default class Viewport extends Component {
   constructor(props) {
@@ -17,16 +18,22 @@ export default class Viewport extends Component {
     this.state = {
       showDraggable: true,
       pan: new Animated.ValueXY(),
+      tworzywa: '',
+      szklo: '',
+      papier: '',
+      bio: '',
+      zmieszane: '',
+      inne: '',
       bin_1: 0,
       bin_2: 0,
       bin_3: 0,
       bin_4: 0,
       bin_5: 0,
       bin_6: 0,
+      binName: '',
       result: 0,
       info: '',
       index: 0,
-      trashes: ['tapeta', 'butelka', 'paragon', 'blistry', 'chusteczka', 'kości']
     };
     
     this.panResponder = PanResponder.create({
@@ -48,67 +55,81 @@ export default class Viewport extends Component {
         
         //kosz 1 - metale i tworzywa sztuczne
         // gesture, trash, props
-        if (this.isDropZone1(gesture)) {
+        
+        // if (this.isDropZone1(gesture)) {
+        //   this.setState({
+        //     showDraggable: false,
+        //     result: this.state.result - 1,
+        //     index: this.state.index + 1,
+        //   });
+        //
+        //   //kosz 2 - szkło
+        // } else if (this.isDropZone2(gesture)) {
+        //   this.setState({
+        //     showDraggable: false,
+        //     result: this.state.result - 1,
+        //     info: 'źle',
+        //     index: this.state.index + 1,
+        //   });
+        //
+        //   //kosz 3 - papier
+        // } else if (this.isDropZone3(gesture)) {
+        //   this.setState({
+        //     showDraggable: false,
+        //     result: this.state.result - 1,
+        //     info: 'źle',
+        //     index: this.state.index + 1,
+        //   });
+        //
+        //   //kosz 4 - bio
+        // } else if (this.isDropZone4(gesture)) {
+        //   this.setState({
+        //     showDraggable: false,
+        //     result: this.state.result - 1,
+        //     info: 'źle',
+        //     index: this.state.index + 1,
+        //   });
+      
+          //warunki
+        if (this.isDropZone1(gesture) && this.renderTrash().props.trashTarget === this.state.binName) {
           this.setState({
             showDraggable: false,
-            result: this.state.result - 1,
             index: this.state.index + 1,
-          });
-          
-          //kosz 2 - szkło
-        } else if (this.isDropZone2(gesture)) {
-          this.setState({
-            showDraggable: false,
-            result: this.state.result - 1,
-            info: 'źle',
-            index: this.state.index + 1,
-          });
-          
-          //kosz 3 - papier
-        } else if (this.isDropZone3(gesture)) {
-          this.setState({
-            showDraggable: false,
-            result: this.state.result - 1,
-            info: 'źle',
-            index: this.state.index + 1,
-          });
-          
-          //kosz 4 - bio
-        } else if (this.isDropZone4(gesture)) {
-          this.setState({
-            showDraggable: false,
-            result: this.state.result - 1,
-            info: 'źle',
-            index: this.state.index + 1,
-          });
-          //kosz 5 - odpady zmieszane
-        } else if (this.isDropZone5(gesture) && this.renderTrash_1().props.trashName === this.state.trashes[this.state.index]) {
-          this.setState({
-            showDraggable: false,
             result: this.state.result + 1,
             info: 'dobrze',
-            index: this.state.index + 1,
+          
           });
-          //kosz 6 - inne
-        } else if (this.isDropZone6(gesture)) {
+          Animated.spring(
+          this.state.pan,
+          {
+            toValue: {x: 0, y: 0}
+          }
+          ).start();
+        }
+        
+        
+        if (this.isDropZone1(gesture) && this.renderTrash().props.trashTarget !== this.state.binName) {
           this.setState({
             showDraggable: false,
-            result: this.state.result - 1,
-            info: 'źle',
             index: this.state.index + 1,
+            info: 'źle',
+          
           });
+          Animated.spring(
+          this.state.pan,
+          {
+            toValue: {x: 0, y: 0}
+          }
+          ).start();
         } else {
           Animated.spring(
           this.state.pan,
-          {toValue: {x: 0, y: 0}}
+          {
+            toValue: {x: 0, y: 0}
+          }
           ).start();
         }
-        console.log(this.renderTrash_1().props.trashName);
-        console.log(this.state.trashes[this.state.index]);
-        console.log(this.state.index);
-        console.log(this.state.result);
-        console.log(this.state.showDraggable);
-        console.log(this.state.trashes);
+        
       }
     });
   }
@@ -116,6 +137,7 @@ export default class Viewport extends Component {
   //20 - margines dla dropzone
   //kosz 1 - metale i tworzywa sztuczne
   isDropZone1(gesture) {
+    console.log(gesture);
     let trash = this.state.bin_1;
     return (
     gesture.moveY > trash.y &&
@@ -184,10 +206,6 @@ export default class Viewport extends Component {
   
   //kosz 6 - inne
   isDropZone6(gesture) {
-    // this.setState({
-    //   testX: gesture.moveX.toFixed(),
-    //   testY: gesture.moveY.toFixed()
-    // });
     let trash = this.state.bin_6;
     return (
     
@@ -200,43 +218,13 @@ export default class Viewport extends Component {
     );
   }
   
-  setDropZoneValues1(event) {
-    console.log(event);
+  setDropZoneValues(param1, param2, e) {
     this.setState({
-      bin_1: event.nativeEvent.layout
+      [param1]: param1,
+      [param2]: e.nativeEvent.layout
     });
   }
-  
-  setDropZoneValues2(event) {
-    this.setState({
-      bin_2: event.nativeEvent.layout
-    });
-  }
-  
-  setDropZoneValues3(event) {
-    this.setState({
-      bin_3: event.nativeEvent.layout
-    });
-  }
-  
-  setDropZoneValues4(event) {
-    this.setState({
-      bin_4: event.nativeEvent.layout
-    });
-  }
-  
-  setDropZoneValues5(event) {
-    this.setState({
-      bin_5: event.nativeEvent.layout
-    });
-  }
-  
-  setDropZoneValues6(event) {
-    this.setState({
-      bin_6: event.nativeEvent.layout
-    });
-  }
-  
+
   render() {
     return (
     <View style={styles.mainContainer}>
@@ -245,47 +233,46 @@ export default class Viewport extends Component {
         <Text>Title {this.state.info}</Text></View>
       <View style={styles.bins}>
         <View style={styles.bins__wrapper}>
-          <Text style={styles.category} onLayout={this.setDropZoneValues1.bind(this)}>xxx1. Metale i tworzywa
+          <Text style={styles.category} onLayout={this.setDropZoneValues.bind(this, 'tworzywa', 'bin_1')}>1. Metale i tworzywa
             sztuczne1</Text>
-          <Text style={styles.category} onLayout={this.setDropZoneValues2.bind(this)}>x2.Szkło</Text>
-          <Text style={styles.category} onLayout={this.setDropZoneValues3.bind(this)}>3.Papier</Text>
-          <Text style={styles.category} onLayout={this.setDropZoneValues4.bind(this)}>4.Bio</Text>
-          <Text style={styles.category} onLayout={this.setDropZoneValues5.bind(this)}>5.Zmieszane</Text>
-          <Text style={styles.category} onLayout={this.setDropZoneValues6.bind(this)}>6.Inne (PSZOK, apteki
+          <Text style={styles.category} onLayout={this.setDropZoneValues.bind(this, 'szklo', 'bin_2')}>xx2.Szkło</Text>
+          <Text style={styles.category} onLayout={this.setDropZoneValues.bind(this, 'papier', 'bin_3')}>3.Papier</Text>
+          <Text style={styles.category} onLayout={this.setDropZoneValues.bind(this, 'bio', 'bin_4')}>4.Bio</Text>
+          <Text style={styles.category} onLayout={this.setDropZoneValues.bind(this, 'zmieszane', 'bin_5')}>5.Zmieszane</Text>
+          <Text style={styles.category} onLayout={this.setDropZoneValues.bind(this, 'inne', 'bin_6')}>6.Inne (PSZOK, apteki
             itp.)6</Text>
         </View>
       </View>
       
       
       <View style={styles.trash}></View>
-      <View style={styles.scores}><Text>{this.state.result} index: {this.state.index}
+      <View style={styles.scores}><Text>punkty: {this.state.result} index: {this.state.index}
       </Text></View>
       
-      {this.renderTrash_1()}
+      {this.renderTrash()}
       
       <View><Text>_</Text></View>
-      <View>
-        <Text> ___________ x: {this.state.bin_5.x} y:{this.state.bin_5.y} h: {this.state.bin_5.height}
-          X:{this.state.testX} Y:{this.state.testY} </Text></View>
+      {/*<View>*/}
+      {/*  <Text> ___________ x: {this.state.bin_5.x} y:{this.state.bin_5.y} h: {this.state.bin_5.height}*/}
+      {/*    X:{this.state.testX} Y:{this.state.testY} </Text></View>*/}
     </View>
     
     
     );
   }
   //kopia funkcji
-  renderTrash_1() {
-    if(this.state.showDraggable) {
+  renderTrash() {
       return (
-      <View style={styles.draggableContainer} trashName={this.state.trashes[this.state.index]}>
+      <View style={styles.draggableContainer} trashTarget={trashesList[this.state.index].target}>
         <Animated.View
         {...this.panResponder.panHandlers}
         style={[this.state.pan.getLayout(), styles.circle]}>
-          <Text style={styles.text}>{this.state.trashes[this.state.index]}</Text>
+          <Text style={styles.text}>{trashesList[this.state.index].name}</Text>
         </Animated.View>
       </View>
       
       )
-    }
+    
   }
 }
 
