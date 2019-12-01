5a85e9a38b82fda4ffaaa5dee7a24a0ba3c0bcc8
Piotr Bartosinski <piotr.bartosinski@outlook.com>
2019-12-01 12:45:49 +0100

    Trashes list

---
+++ Trashes/trashes_list.js
@@ -0,0 +1,107 @@
+export default [
+  {
+    id: 1,
+    name: 'butelka',
+    target: 'szklo',
+    img: require('./img/trashes/trash_1.png'),
+    color: '#38a628'
+  },
+  {
+    id: 2,
+    name: 'karton',
+    target: 'papier',
+    img: require('./img/trashes/trash_2.png'),
+    color: '#0068c9'
+  },
+  {
+    id: 3,
+    name: 'paragon',
+    target: 'zmieszane',
+    img: require('./img/trashes/trash_3.png'),
+    color: '#202020'
+  },
+  {
+    id: 4,
+    name: 'stary lek',
+    target: 'inne',
+    img: require('./img/trashes/trash_4.png'),
+    color: '#FC702C'
+  },
+  {
+    id: 5,
+    name: 'kość',
+    target: 'zmieszane',
+    img: require('./img/trashes/trash_5.png'),
+    color: '#202020'
+  },
+  {
+    id: 6,
+    name: 'skorupka jaja',
+    target: 'bio',
+    img: require('./img/trashes/trash_6.png'),
+    color: '#8E5731'
+  },
+  {
+    id: 7,
+    name: 'karton po soku',
+    target: 'tworzywa',
+    img: require('./img/trashes/trash_7.png'),
+    color: '#FFB11C'
+  },
+  {
+    id: 8,
+    name: 'zwiędły kwiat',
+    target: 'bio',
+    img: require('./img/trashes/trash_8.png'),
+    color: '#8E5731'
+  },
+  {
+    id: 9,
+    name: 'zużyta chusteczka',
+    target: 'zmieszane',
+    img: require('./img/trashes/trash_9.png'),
+    color: '#202020'
+  },
+  {
+    id: 10,
+    name: 'dywan',
+    target: 'inne',
+    img: require('./img/trashes/trash_10.png'),
+    color: '#FC702C'
+  },
+  {
+    id: 11,
+    name: 'fusy po kawie',
+    target: 'bio',
+    img: require('./img/trashes/trash_11.png'),
+    color: '#8E5731'
+  },
+  {
+    id: 12,
+    name: 'niezjedzona kanapka',
+    target: 'zmieszane',
+    img: require('./img/trashes/trash_12.png'),
+    color: '#202020'
+  },
+  {
+    id: 13,
+    name: 'worek foliowy',
+    target: 'tworzywa',
+    img: require('./img/trashes/trash_13.png'),
+    color: '#FFB11C'
+  },
+  {
+    id: 14,
+    name: 'styropian',
+    target: 'zmieszane',
+    img: require('./img/trashes/trash_14.png'),
+    color: '#202020'
+  },
+  {
+    id: 15,
+    name: 'puszka',
+    target: 'tworzywa',
+    img: require('./img/trashes/trash_15.png'),
+    color: '#FFB11C'
+  },
+];
